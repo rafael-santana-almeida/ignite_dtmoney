4e27ca4d07c203752bca43bf32356f49364701ba
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-01-16 17:13:33 -0300

    Create TransactionsTable component

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.section`
     background: var(--shape);
     padding: 1.5rem 2rem;
     border-radius: 0.25rem;
-    color:  var(--text-title);
+    color: var(--text-title);
 
     header {
       display: flex;
