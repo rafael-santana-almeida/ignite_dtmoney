3bf5683982b067aeab6cbfb810999232dd327381
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-01-16 15:47:34 -0300

    Create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>dtmoney</h1>
+    <div className="App">
+      <h1>dtmoney</h1>
+
+      <GlobalStyle />
+    </div>
   );
 }
