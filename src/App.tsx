f7209fc3003f1e3f5b56536650765d9e65a95708
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-01-29 14:56:40 -0300

    Create NewTransactionModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransationModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransationModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransationModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
