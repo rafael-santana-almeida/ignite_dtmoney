859809483c802ce742f8ee0f28e5dd1fc6ccecf4
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-01-29 15:43:47 -0300

    Create type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import Modal from "react-modal";
 
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 Modal.setAppElement("#root");
 
@@ -34,7 +36,21 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
 
         <input type="text" placeholder="Titulo" />
+
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
